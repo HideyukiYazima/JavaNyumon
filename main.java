ccea481863764db71da6f537ecc9e0365d7322ad
Yazima <yazima04200@gmail.com>
2023-08-04 17:32:55 +0900

    上書き保存

---
+++ main.java
@@ -0,0 +1,50 @@
+// class main {
+//   public static void main(String[] args) {
+//     System.out.println(17);
+//     System.out.println(5 + 3);
+//     System.out.println("5 - 3");
+//   }
+// }
+
+
+class main {
+  public static void main(String[] args) {
+    System.out.println(12 / 3);
+    System.out.println(3 * 6);
+    System.out.println("8 % 3");
+  }
+}
+
+
+class main {
+  public static void main(String[] args) {
+    System.out.println("こんにちは" + "世界");
+    System.out.println("38" + "19");
+    System.out.println(38 + 19);
+  }
+}
+
+class main {
+  public static void main(String[] args) {
+    int number;
+    number = 3;
+    System.out.println(number);
+
+    String name;
+    name = "Sato";
+    System.out.println(name);
+  }
+}
+
+class main {
+  public static void main(String[] args) {
+    int number1 = 3;
+    int number2 = 7;
+    System.out.println(number1 * number2);
+
+    String text = "プログラミングを勉強しよう";
+    System.out.println("独学で" + text);
+  }
+}
+
+
