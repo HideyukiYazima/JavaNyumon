805b63b75b909b5400e2cebe0bca026408b14429
Yazima <yazima04200@gmail.com>
2023-08-06 21:58:43 +0900

    上書き保存

--- main.java
+++ main.java
@@ -7,68 +7,68 @@
 // }
 
 
-class main {
-  public static void main(String[] args) {
-    System.out.println(12 / 3);
-    System.out.println(3 * 6);
-    System.out.println("8 % 3");
-  }
-}
+// class main {
+//   public static void main(String[] args) {
+//     System.out.println(12 / 3);
+//     System.out.println(3 * 6);
+//     System.out.println("8 % 3");
+//   }
+// }
 
 
-class main {
-  public static void main(String[] args) {
-    System.out.println("こんにちは" + "世界");
-    System.out.println("38" + "19");
-    System.out.println(38 + 19);
-  }
-}
+// class main {
+//   public static void main(String[] args) {
+//     System.out.println("こんにちは" + "世界");
+//     System.out.println("38" + "19");
+//     System.out.println(38 + 19);
+//   }
+// }
 
-class main {
-  public static void main(String[] args) {
-    int number;
-    number = 3;
-    System.out.println(number);
+// class main {
+//   public static void main(String[] args) {
+//     int number;
+//     number = 3;
+//     System.out.println(number);
 
-    String name;
-    name = "Sato";
-    System.out.println(name);
-  }
-}
+//     String name;
+//     name = "Sato";
+//     System.out.println(name);
+//   }
+// }
 
-class main {
-  public static void main(String[] args) {
-    int number1 = 3;
-    int number2 = 7;
-    System.out.println(number1 * number2);
+// class main {
+//   public static void main(String[] args) {
+//     int number1 = 3;
+//     int number2 = 7;
+//     System.out.println(number1 * number2);
 
-    String text = "プログラミングを勉強しよう";
-    System.out.println("独学で" + text);
-  }
-}
+//     String text = "プログラミングを勉強しよう";
+//     System.out.println("独学で" + text);
+//   }
+// }
 
-class Main {
-  public static void main(String[] args) {
-    int number = 11;
-    String text = "夢";
-    System.out.println(number);
-    System.out.println(text);
-    number = 9;
-    System.out.println(number);
-    text = "幻想";
-    System.out.println(text);
+// class Main {
+//   public static void main(String[] args) {
+//     int number = 11;
+//     String text = "夢";
+//     System.out.println(number);
+//     System.out.println(text);
+//     number = 9;
+//     System.out.println(number);
+//     text = "幻想";
+//     System.out.println(text);
     
-  }
-}
+//   }
+// }
 
-class Main {
-  public static void main(String[] args) {
-    int number = 3;
-    System.out.println(number);
-    number = number + 7; 
-    System.out.println(number);
-  }
-}
+// class Main {
+//   public static void main(String[] args) {
+//     int number = 3;
+//     System.out.println(number);
+//     number = number + 7; 
+//     System.out.println(number);
+//   }
+// }
 
 class Main {
   public static void main(String[] args) {
